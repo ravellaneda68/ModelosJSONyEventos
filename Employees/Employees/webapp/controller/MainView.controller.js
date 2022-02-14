b13fd7625c8cec397cb2cd8eb5d42b0eba91450b
ravellaneda68 <your@example.comramiro.avellaneda@gmail.comgit config --global user.email ramiro.avellaneda@gmail.comgit config --global user.name Yoravellaneda68>
2022-02-14 16:39:04 +0000

    ModelosJSONyEventos

--- Employees/Employees/webapp/controller/MainView.controller.js
+++ Employees/Employees/webapp/controller/MainView.controller.js
@@ -1,11 +1,44 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
 ],
     /**
     * @param {typeof sap.ui.core.mvc.Controller} Controller
     */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
+
+        function onInit() {
+
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
+
+            //oJSONModel.setData(oJSON);
+            oJSONModel.loadData("./localService/mockdata/Employees.json", false);
+            oView.setModel(oJSONModel);
+        };
+        function onFilter() {
+            var oJSON = this.getView().getModel().getData();
+            var filters = [];
+            if (oJSON.EmployeeId !== "") {
+                filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
+            }
+            if (oJSON.CountryKey !== "") {
+                filters.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
+            }
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+        };
+        function onClearFilter() {
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
+
+        }
+
         var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
 
         Main.prototype.onValidate = function () {
@@ -22,5 +55,9 @@ sap.ui.define([
                     ;
             }
         };
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter;
+        Main.prototype.onClearFilter = onClearFilter;
         return Main;
+
     });
